d8c1aa7b3375aedf6fae9103eb4d3e7e47f4025a
ntp0102 <phuc.nguyenthien2000@hcmut.edu.vn>
2023-03-01 00:23:43 +0700

    update1

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -28,7 +28,7 @@ let getWebhook = (req, res) => {
   // Check if a token and mode is in the query string of the request
   if (mode && token) {
     // Check the mode and token sent is correct
-    if (mode === "subscribe" && token === config.verifyToken) {
+    if (mode === "subscribe" && token === config.VERIFY_TOKEN) {
       // Respond with the challenge token from the request
       console.log("WEBHOOK_VERIFIED");
       res.status(200).send(challenge);
