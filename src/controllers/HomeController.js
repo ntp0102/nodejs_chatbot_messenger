003014cf93f9f64e272095d24cd419d27936790a
ntp0102 <phuc.nguyenthien2000@hcmut.edu.vn>
2023-02-28 23:49:47 +0700

    init project

---
+++ src/controllers/HomeController.js
@@ -0,0 +1,46 @@
+require("dotenv").config();
+
+let getHomePage = (req, res) => {
+  return res.render("homepage.ejs");
+};
+
+let postWebhook = (req, res) => {
+  let body = req.body;
+  console.log(body);
+  console.log(`\u{1F7EA} Received webhook:`);
+  console.dir(body, { depth: null });
+
+  if (body.object === "page") {
+    res.status(200).send("EVENT_RECEIVED");
+
+    // Determine which webhooks were triggered and get sender PSIDs and locale, message content and more.
+  } else {
+    res.sendStatus(404);
+  }
+};
+
+let getWebhook = (req, res) => {
+  // Parse the query params
+  let mode = req.query["hub.mode"];
+  let token = req.query["hub.verify_token"];
+  let challenge = req.query["hub.challenge"];
+
+  // Check if a token and mode is in the query string of the request
+  if (mode && token) {
+    // Check the mode and token sent is correct
+    if (mode === "subscribe" && token === config.verifyToken) {
+      // Respond with the challenge token from the request
+      console.log("WEBHOOK_VERIFIED");
+      res.status(200).send(challenge);
+    } else {
+      // Respond with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403);
+    }
+  }
+};
+
+module.exports = {
+  getHomePage: getHomePage,
+  postWebhook: postWebhook,
+  getWebhook: getWebhook,
+};
