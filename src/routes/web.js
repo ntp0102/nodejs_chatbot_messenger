d5e9c1c4b94de10849eda72d594961a8bd4564fa
ntp0102 <phuc.nguyenthien2000@hcmut.edu.vn>
2023-03-01 15:44:35 +0700

    update1

--- src/routes/web.js
+++ src/routes/web.js
@@ -6,8 +6,8 @@ let router = express.Router();
 let initWebRoutes = (app) => {
   router.get("/", HomeController.getHomePage);
 
-  router.post("/webhook", HomeController.postWebhook);
-  router.get("/messaging-webhook", HomeController.getWebhook);
+  // router.post("/webhook", HomeController.postWebhook);
+  // router.get("/messaging-webhook", HomeController.getWebhook);
   router.post("/test", HomeController.test);
   router.post("/about", (req, res) => {
     let a = req.body;
